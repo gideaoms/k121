bc715d779ebd59bf5414128093e57d6741f25535
Gideão Silva <gideaoms@gmail.com>
2018-05-19 17:05:42 -0300

    static express

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,9 +7,12 @@ const { url } = require('./config/database');
 const { port, origin } = require('./config/app');
 const routes = require('./app/routes');
 
+const app = express();
+
 const start = async () => {
   await mongoose.connect(url);
 
+  app.use(express.static(path.resolve('../', 'frontend', 'public')));
   app.use(cors({ origin }));
   app.use(bodyParser.json());
 
